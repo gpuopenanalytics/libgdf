048aa5d33d4ed4bce953c08c5fe1d9f0f1e6b709
Brad Rees <brees@nvidia.com>
2018-07-30 16:41:16 -0400

    CSV Reader API

--- include/gdf/gdf.h
+++ include/gdf/gdf.h
@@ -4,11 +4,13 @@
 #include <cstdlib>
 #include <cstdint>
 #include "cffi/types.h"
+#include "cffi/csv.h"
 
 #define GDF_VALID_BITSIZE (sizeof(gdf_valid_type) * 8)
 
 extern "C" {
 #include "cffi/functions.h"
+#include "cffi/io_functions.h"
 }
 
 #endif /* GDF_GDF_H */
