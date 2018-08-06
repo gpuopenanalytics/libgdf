51094aa4403b5187174eddb5a1989d7575e0920d
Brad Rees <brees@nvidia.com>
2018-08-06 16:04:35 -0400

    fixed bad data types

--- include/gdf/cffi/io_types.h
+++ include/gdf/cffi/io_types.h
@@ -28,8 +28,8 @@ typedef struct {
 	/*
 	 * Output Arguments - space created in reader.
 	 */
-	const int		*num_cols;							// Out: number of columns
-	const int		*num_rows;							// Out: number of rows
+	int				num_cols;							// Out: number of columns
+	int				num_rows;							// Out: number of rows
 	gdf_column		**data;								// Out: the gdf_columns
 
 
@@ -42,8 +42,8 @@ typedef struct {
 
 	const char		lineterminator		= '\n';			// can change the end of line character
 	const char		delimiter			= ',';			// also called 'sep'  this is the field separator
-	const boolean 	delim_whitespace	= false;		// use white space as the delimiter
-	const boolean	skipinitialspace	= false;		// Skip spaces after delimiter
+	const bool 		delim_whitespace	= false;		// use white space as the delimiter
+	const bool		skipinitialspace	= false;		// Skip spaces after delimiter
 
 	const int		nrows				= -1;			// number of rows to read,  -1 indicates all
 
@@ -58,18 +58,18 @@ typedef struct {
 	const int		**skiprows			= NULL;			// array of int		number of rows at the start of the files to skip
 	const int		**skipfooter		= NULL;			// array of int		number of rows at the bottom of the file to skip - counting is backwards from end, 0 = last line
 
-	const boolean	skip_blank_lines	= true;			// whether or not to ignore blank lines
+	const bool		skip_blank_lines	= true;			// whether or not to ignore blank lines
 
 	const char		*prefix				= NULL;			// if there is no header or names, attend this to the column ID as the name
-	const boolean	mangle_dupe_cols	= false;		// if true: duplicate columns will be specified as ‘X’, ‘X.1’, …’X.N’, rather than ‘X’…’X’  - CSV reader does not utilize column name for processing
+	const bool		mangle_dupe_cols	= false;		// if true: duplicate columns will be specified as ‘X’, ‘X.1’, …’X.N’, rather than ‘X’…’X’  - CSV reader does not utilize column name for processing
 
 	const char		**true_values		= NULL;			// array of char *	what should be considered as True - each char string contains {col ID, true value, ...} - this will allow multiple true values to be specified over multiple columns
 	const char		**false_values		= NULL;			// array of char *	what should be considered as True - each char string contains {col ID, false value, ...} - this will allow multiple false values to be specified over multiple columns
 	const char		**na_values			= NULL;			// array of char *	what should be considered as True - each char string contains {col ID, na value, ...} - this will allow multiple na values to be specified over multiple columns
 
-	const boolen	parse_dates			= true;			// parse date field into date32 or date64.  If false then date fields are saved as a string. Specifying a date dtype overrides this
-	const boolean	infer_datetime_format = true;		// try and determine the date format
-	const boolean	dayfirst			= false;		// is the first value the day?  DD/MM  versus MM/DD
+	const bool		parse_dates			= true;			// parse date field into date32 or date64.  If false then date fields are saved as a string. Specifying a date dtype overrides this
+	const bool		infer_datetime_format = true;		// try and determine the date format
+	const bool		dayfirst			= false;		// is the first value the day?  DD/MM  versus MM/DD
 
 	const char		*compression		= NULL;			// specify the type of compression
 
@@ -78,7 +78,7 @@ typedef struct {
 	const char		decimal				= '.';			// the decimal point character
 
 	const char		*quotechar			= NULL;			// single character 	character to use as a quote
-	const boolean	doublequote			= true;			// whether to treat two quotes as a quote in a string or empty
+	const bool		doublequote			= true;			// whether to treat two quotes as a quote in a string or empty
 
 	const char		*escapechar			= NULL;			// single char	- char used as the escape character
 
