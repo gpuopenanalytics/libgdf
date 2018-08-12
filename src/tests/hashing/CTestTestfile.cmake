bfcef56a8338e3e3163951cb9e8157d52e510d1d
mt-jones <majones@nvidia.com>
2018-08-12 17:11:42 -0600

    Removing extraneous files

--- src/tests/hashing/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mt_jones/repos/libgdf/src/tests/hashing
-# Build directory: /home/mt_jones/repos/libgdf/src/tests/hashing
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(hash_test "/home/mt_jones/repos/libgdf/gtests/hash_test")
