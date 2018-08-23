0b62cd9796eedf070a98d591e00d8f65bc5d39b2
gcca <cristhian@blazingdb.com>
2018-08-23 18:10:59 -0500

    [replace-function] Add replacement by lower bound

--- src/replace.cu
+++ src/replace.cu
@@ -15,9 +15,10 @@
  * limitations under the License.
  */
 
+#include <thrust/binary_search.h>
 #include <thrust/device_ptr.h>
 #include <thrust/device_vector.h>
-#include <thrust/replace.h>
+#include <thrust/for_each.h>
 
 #include <gdf/gdf.h>
 
@@ -45,21 +46,50 @@ DTYPE_FACTORY(TIMESTAMP, std::int64_t);
 #undef DTYPE_FACTORY
 
 template <class T>
+class ReplaceFunctor {
+public:
+    ReplaceFunctor(T *const raw) : data(raw) {}
+
+    __host__ __device__ void
+             operator()(thrust::tuple<const T, const T, const T> tuple) {
+        const T i = thrust::get<0>(tuple);
+
+        if (data[i] == thrust::get<1>(tuple)) {
+            data[i] = thrust::get<2>(tuple);
+        }
+    }
+
+    thrust::device_ptr<T> data;
+};
+
+template <class T>
 static inline void
 Replace(T *const          data,
         const std::size_t data_size,
-        T *const          to_replace,
-        T *const          values,
+        const T *const    to_replace,
+        const T *const    values,
         const std::size_t replacement_size) {
-    thrust::device_ptr<T> begin(data);
-    thrust::device_ptr<T> end = begin + static_cast<std::ptrdiff_t>(data_size);
+    const thrust::device_ptr<const T> data_begin(data);
+    const thrust::device_ptr<const T> data_end =
+      data_begin + static_cast<std::ptrdiff_t>(data_size);
 
-    thrust::device_ptr<T> from(to_replace);
-    thrust::device_ptr<T> to(values);
+    const thrust::device_ptr<const T> from_begin(to_replace);
+    const thrust::device_ptr<const T> from_end =
+      from_begin + static_cast<std::ptrdiff_t>(replacement_size);
 
-    for (std::size_t i = 0; i < replacement_size; i++) {
-        thrust::replace(begin, end, from[i], to[i]);
-    }
+    const thrust::device_ptr<const T> to_begin(values);
+    const thrust::device_ptr<const T> to_end =
+      to_begin + static_cast<std::ptrdiff_t>(replacement_size);
+
+    thrust::device_vector<T> lower_bounds(replacement_size);
+    thrust::lower_bound(
+      data_begin, data_end, from_begin, from_end, lower_bounds.begin());
+
+    thrust::for_each(thrust::make_zip_iterator(thrust::make_tuple(
+                       lower_bounds.cbegin(), from_begin, to_begin)),
+                     thrust::make_zip_iterator(thrust::make_tuple(
+                       lower_bounds.cend(), from_end, to_end)),
+                     ReplaceFunctor<T>(data));
 }
 
 static inline bool
