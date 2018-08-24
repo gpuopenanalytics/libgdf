5f2c338d7407a6250714b02eb0986382b16c2be9
gcca <cristhian@blazingdb.com>
2018-08-23 19:10:15 -0500

    [replace-function] Update typed unit test

--- src/tests/replace/replace-test.cu
+++ src/tests/replace/replace-test.cu
@@ -21,6 +21,24 @@
 
 #include <gdf/gdf.h>
 
+template <class U>
+struct TypeTraits {};
+
+#define TYPE_FACTORY(U, D)                                                    \
+    template <>                                                               \
+    struct TypeTraits<U> {                                                    \
+        static constexpr gdf_dtype dtype = GDF_##D;                           \
+    }
+
+TYPE_FACTORY(std::int8_t, INT8);
+TYPE_FACTORY(std::int16_t, INT16);
+TYPE_FACTORY(std::int32_t, INT32);
+TYPE_FACTORY(std::int64_t, INT64);
+TYPE_FACTORY(float, FLOAT32);
+TYPE_FACTORY(double, FLOAT64);
+
+#undef TYPE_FACTORY
+
 template <class T>
 static inline thrust::device_vector<T>
 MakeDeviceVector(const std::initializer_list<T> list) {
@@ -29,27 +47,35 @@ MakeDeviceVector(const std::initializer_list<T> list) {
     return device_data;
 }
 
+template <class T>
 static inline gdf_column
-MakeGdfColumn(thrust::device_vector<std::int64_t> &device_vector) {
+MakeGdfColumn(thrust::device_vector<T> &device_vector) {
     return gdf_column{
       .data       = thrust::raw_pointer_cast(device_vector.data()),
       .valid      = nullptr,
       .size       = device_vector.size(),
-      .dtype      = GDF_INT64,
+      .dtype      = TypeTraits<T>::dtype,
       .null_count = 0,
       .dtype_info = {},
     };
 }
 
-TEST(ReplaceTest, API) {
-    thrust::device_vector<std::int64_t> device_data =
-      MakeDeviceVector<std::int64_t>({1, 2, 3, 4, 5, 6, 7, 8});
+template <class T>
+class ReplaceTest : public testing::Test {};
+
+using Types = testing::
+  Types<std::int8_t, std::int16_t, std::int32_t, std::int64_t, float, double>;
+TYPED_TEST_CASE(ReplaceTest, Types);
+
+TYPED_TEST(ReplaceTest, ReplaceEvenPosition) {
+    thrust::device_vector<TypeParam> device_data =
+      MakeDeviceVector<TypeParam>({1, 2, 3, 4, 5, 6, 7, 8});
     gdf_column column = MakeGdfColumn(device_data);
 
-    thrust::device_vector<std::int64_t> to_replace_data =
-      MakeDeviceVector<std::int64_t>({2, 4, 6, 8});
-    thrust::device_vector<std::int64_t> values_data =
-      MakeDeviceVector<std::int64_t>({0, 2, 4, 6});
+    thrust::device_vector<TypeParam> to_replace_data =
+      MakeDeviceVector<TypeParam>({2, 4, 6, 8});
+    thrust::device_vector<TypeParam> values_data =
+      MakeDeviceVector<TypeParam>({0, 2, 4, 6});
 
     gdf_column to_replace = MakeGdfColumn(to_replace_data);
     gdf_column values     = MakeGdfColumn(values_data);
@@ -58,8 +84,8 @@ TEST(ReplaceTest, API) {
 
     EXPECT_EQ(GDF_SUCCESS, status);
 
-    thrust::device_ptr<std::int64_t> results(
-      static_cast<std::int64_t *>(column.data));
+    thrust::device_ptr<TypeParam> results(
+      static_cast<TypeParam *>(column.data));
     EXPECT_EQ(0, results[1]);
     EXPECT_EQ(2, results[3]);
     EXPECT_EQ(4, results[5]);
